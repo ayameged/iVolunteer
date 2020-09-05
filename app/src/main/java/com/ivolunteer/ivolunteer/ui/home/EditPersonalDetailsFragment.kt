ffa60c57be2d4a32c27aa6557f985be10ef7fd91
Aya Meged <megedaya@gmail.com>
2020-09-06 00:11:33 +0300

    Merge branch 'master' of C:\Users\meged\AndroidStudioProjects\iVolunteer with conflicts.

---
+++ app/src/main/java/com/ivolunteer/ivolunteer/ui/home/EditPersonalDetailsFragment.kt
@@ -0,0 +1,31 @@
+package com.ivolunteer.ivolunteer.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.ivolunteer.ivolunteer.R
+
+class EditPersonalDetailsFragment : Fragment() {
+
+  private lateinit var homeViewModel: EditPersonalDetailsModel
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    homeViewModel =
+    ViewModelProviders.of(this).get(EditPersonalDetailsModel::class.java)
+    val root = inflater.inflate(R.layout.fragment_edit_personal_details, container, false)
+    val textView: TextView = root.findViewById(R.id.text_home)
+    homeViewModel.text.observe(viewLifecycleOwner, Observer {
+      textView.text = it
+    })
+    return root
+  }
+}
