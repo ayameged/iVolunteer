99ae130de8bf988c23ec9c584f48a9fb10b33867
admin <meitalanid@gmail.com>
2020-09-07 23:52:43 +0300

    Add MyActivities ListView

---
+++ app/src/main/java/com/ivolunteer/ivolunteer/ui/needHelpUserEditPersonalDetails/MyListAdapter.kt
@@ -0,0 +1,27 @@
+package com.ivolunteer.ivolunteer.ui.needHelpUserEditPersonalDetails
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.ivolunteer.ivolunteer.R
+
+class MyListAdapter(private val context: Activity, private val type: Array<String?>, private val city: Array<String?>)
+    : ArrayAdapter<String>(context, R.layout.custom_list, type) {
+
+    override fun getView(position: Int, view: View?, parent: ViewGroup): View {
+        val inflater = context.layoutInflater
+        val rowView = inflater.inflate(R.layout.custom_list, null, true)
+
+        val titleText = rowView.findViewById(R.id.type) as TextView
+        val subtitleText = rowView.findViewById(R.id.city) as TextView
+
+        titleText.text = type[position]
+        subtitleText.text = city[position]
+
+        return rowView
+    }
+}
--- app/src/main/java/com/ivolunteer/ivolunteer/ui/needHelpUserMyActivities/MyActivitiesFragment.kt
+++ app/src/main/java/com/ivolunteer/ivolunteer/ui/needHelpUserMyActivities/MyActivitiesFragment.kt
@@ -1,22 +1,20 @@
 package com.ivolunteer.ivolunteer.ui.needHelpUserMyActivities
 
+import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ivolunteer.ivolunteer.R
 import com.ivolunteer.ivolunteer.resources.NetworkManager
 import com.ivolunteer.ivolunteer.resources.StorageManager
 import com.ivolunteer.ivolunteer.resources.StorageTypes
-import com.ivolunteer.ivolunteer.types.City
 import com.ivolunteer.ivolunteer.types.needhelpuseractivities.NeedHelpUserActivities
+import com.ivolunteer.ivolunteer.ui.needHelpUserEditPersonalDetails.MyListAdapter
 
 class MyActivitiesFragment : Fragment() {
 
@@ -31,15 +29,65 @@ class MyActivitiesFragment : Fragment() {
       ViewModelProviders.of(this).get(GalleryViewModel::class.java)
     val root = inflater.inflate(R.layout.fragment_my_activities, container, false)
 
-    var listView = view?.findViewById<ListView>(R.id.recipe_list_view)
+    var listView = root.findViewById<ListView>(R.id.recipe_list_view)
+   // val listView = view?.findViewById(android.R.id.recipe_list_view)
 
-    NetworkManager.instance.get<NeedHelpUserActivities>("volunteers/byuserid?id="+StorageManager.instance.get<String>(StorageTypes.USER_ID.toString())) { response, statusCode, error ->
+    NetworkManager.instance.get<NeedHelpUserActivities>(
+      "volunteers/byuserid?id=" + StorageManager.instance.get<String>(
+        StorageTypes.USER_ID.toString()
+      )
+    ) { response, statusCode, error ->
       if (statusCode == 200){
         //        TODO: Meital - list
-//        response[0].details
+//
 
-      }else{
-        Log.i("LOG - failed to get activities ", error.toString())
+
+       // response[0].details
+        val details=arrayOfNulls<String>(response!!.size)
+        val type=arrayOfNulls<String>(response!!.size)
+        for(i in 0 until response!!.size) {
+          details[i] = (response[i].details)
+          type[i] = (response[i].volunteerType.type)
+        }
+
+
+//custom list - not working -
+/*
+        val myListAdapter =activity?.applicationContext?.let { MyListAdapter(it as Fragment,type,details)}
+
+
+        if (listView != null) {
+          listView.post {
+
+
+            listView!!.adapter = myListAdapter
+
+          }
+        }
+*/
+        //working
+
+          val adapter = activity?.applicationContext?.let {
+            ArrayAdapter<String>(
+              it, android.R.layout.simple_list_item_1, type)
+          }
+
+
+        if (listView != null) {
+          listView.post {
+
+
+            listView!!.adapter = adapter
+
+          }
+        }
+
+
+
+
+
+        }else{
+        //Log.i("LOG - failed to get activities ", error.toString())
       }
 
     }
